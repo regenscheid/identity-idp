df374f2ea5c69c2c96dea68dd87a1ff12acaf951
Peter Karman <peter.karman@gsa.gov>
2016-10-04 16:29:33 -0500

    add missing specs

---
+++ spec/controllers/mfa_confirmation_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe MfaConfirmationController do
+  describe 'GET /reauthn' do
+    it 'presents the password confirmation form' do
+      stub_sign_in
+
+      get :new
+
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'PUT /reauthn' do
+    let(:user) { create(:user, :signed_up, password: 'password') }
+
+    before do
+      stub_sign_in(user)
+    end
+
+    context 'password is empty' do
+      it 'redirects with error message' do
+        post :create
+
+        expect(response).to redirect_to(user_password_confirm_path)
+        expect(flash[:error]).to eq t('errors.confirm_password_incorrect')
+      end
+
+      it 'redirects with error message' do
+        post :create, user: { password: '' }
+
+        expect(response).to redirect_to(user_password_confirm_path)
+        expect(flash[:error]).to eq t('errors.confirm_password_incorrect')
+      end
+    end
+
+    context 'password is wrong' do
+      it 'redirects with error message' do
+        post :create, user: { password: 'wrong' }
+
+        expect(response).to redirect_to(user_password_confirm_path)
+        expect(flash[:error]).to eq t('errors.confirm_password_incorrect')
+      end
+    end
+
+    context 'password is correct' do
+      it 'redirects to 2FA' do
+        post :create, user: { password: 'password' }
+
+        expect(response).to redirect_to(user_two_factor_authentication_path(reauthn: true))
+      end
+    end
+  end
+end
